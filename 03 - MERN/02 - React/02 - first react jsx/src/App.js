0ac21b038275c0c319ce05d0587d9b8249b0036f
Jerson Paniagua <diazpany@gmail.com>
2022-12-20 22:18:36 -0300

    add react app

---
+++ 03 - MERN/02 - React/02 - first react jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
