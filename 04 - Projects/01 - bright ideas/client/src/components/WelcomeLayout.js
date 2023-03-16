ba3d7ef2e5ea5e90e967980a59f812e0a19ff512
marcosroman <marcosdromand@gmail.com>
2023-03-16 18:05:07 -0300

    ultimo fix de ui

--- 04 - Projects/01 - bright ideas/client/src/components/WelcomeLayout.js
+++ 04 - Projects/01 - bright ideas/client/src/components/WelcomeLayout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './Authentication/Login';
 import SignUp from './Authentication/SignUp';
 import { navBgColor, mainBgColor } from './Style';
+import einsteinquote from "../einsteinquote.png";
 
 const Layout =(props) =>{
 
@@ -10,6 +11,7 @@ const Layout =(props) =>{
       <div className="flex m-3 p-1 rounded" style={{backgroundColor:mainBgColor, color:"white"}}>
         <h2 className="my-3">Welcome to Bright Ideas</h2>
       </div>
+			<img className="rounded img-fluid" style={{maxWidth: "60%", height: "auto"}} src={einsteinquote}/>
       <div className='d-flex rounded m-3' style={{backgroundColor:navBgColor, border:"1px solid black", margin: "0px 5rem"}}>
         <SignUp />
         <Login />
